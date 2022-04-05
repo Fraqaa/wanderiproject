5661dcf9eebadee05617639ce93496b772105046
Fraqaa <wainainaf526@gmail.com>
2022-04-05 21:05:01 +0300

    wanderwork

---
+++ form3.js
@@ -0,0 +1,12 @@
+// form loading Animation
+
+const form = [...document.querySelector('.form').children];
+
+form.forEach((item, i) => {
+    setTimeout(() => {
+        item.style.opacity = 1;
+
+    }, i*100);
+
+
+})
